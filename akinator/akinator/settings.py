cabfbe0f6df0cec51e7c2262679df4195396e53a
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-10-05 01:53:03 +0300

    Configured static url

--- akinator/akinator/settings.py
+++ akinator/akinator/settings.py
@@ -81,7 +81,7 @@ PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/application/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
