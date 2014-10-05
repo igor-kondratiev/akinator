ad4e04b71417b017f4b8be528b9d43b553f723f2
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-10-05 12:11:26 +0300

    Logging configured

--- akinator/akinator/settings.py
+++ akinator/akinator/settings.py
@@ -87,3 +87,36 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, '..', 'client', 'client_dist'),
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'akinator': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
