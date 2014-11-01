0d4fbae7ece5faaf24e2cd38f0262506082fc569
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-11-02 01:34:25 +0200

    Fixed urls

--- akinator/core/urls.py
+++ akinator/core/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url('game/start/', 'core.views.start_game', name='start_game'),
+    url('game/end/', 'core.views.start_game', name='end_game'),
     url('game/next/', 'core.views.process_response', name='process_response'),
     url('game/stats/', 'core.views.current_stats', name='game_stats'),
     url('statistics/', 'core.views.statistics', name='statistics'),
