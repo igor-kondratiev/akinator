3cc4af1ba92536f9b71b0f11a797b87020fabe8c
RathaR <ivan.pecharitsa@gmail.com>
2014-11-03 22:22:31 +0200

    Fix stats request

--- client/client_dist/scripts/scripts.277def46.js
+++ client/client_dist/scripts/scripts.277def46.js
@@ -860,52 +860,55 @@ angular.module('controllers')
         }
     }]);
 angular.module('services')
-    .service('ApiClient',['$http', function($http) {
+    .service('ApiClient', ['$http', function ($http) {
         return {
-           getStats: function(params) {
+            getStats: function (params) {
                 return $http.get('/api/game/stats/',
                     {
-                        sessionId: params.sessionId,
-                        count: params.count
-                    }).success(function() {
+                        params: {
+                            sessionId: params.sessionId,
+                            count: params.count
+
+                        }
+                    }).success(function () {
 
                     })
-                    .then(function(result) {
-                       return result.data;
-                    });
-            },
-            getStatistics: function(params) {
-                return $http.get('/api/statistics')
-                    .success(function() {
-
-                    }).then(function(result) {
+                    .then(function (result) {
                         return result.data;
                     });
             },
-            startSession: function() {
-                return $http.get('/api/game/start').success(function() {
+            getStatistics: function (params) {
+                return $http.get('/api/statistics')
+                    .success(function () {
 
-                }).then(function(result) {
+                    }).then(function (result) {
+                        return result.data;
+                    });
+            },
+            startSession: function () {
+                return $http.get('/api/game/start').success(function () {
+
+                }).then(function (result) {
                     return result.data;
                 });
             },
-            getQuestion: function(sessionId, answer) {
+            getQuestion: function (sessionId, answer) {
                 return $http.post('/api/game/next', {
                     sessionId: sessionId,
                     answer: answer
-                }).success(function() {
+                }).success(function () {
 
-                }).then(function(result) {
+                }).then(function (result) {
                     return result.data;
                 })
             },
-            makeLastAnswer: function(params) {
+            makeLastAnswer: function (params) {
                 return $http.post('/api/game/end', params)
-                    .success(function(){
+                    .success(function () {
 
-                }).then(function(result) {
-                    return result.data;
-                })
+                    }).then(function (result) {
+                        return result.data;
+                    })
             }
         }
     }]);
